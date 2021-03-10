9e04d947f1b5ba89ae4da1599493683963357039
arjun <priyavrathtiwariluck@gmail.com>
2021-03-10 19:21:18 +0530

    second

---
+++ work2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    if (a>5){
+        printf("u need ticket for entry");
+    }else
+    printf("ur entry is free");
+    
+    return 0;
+
+}
